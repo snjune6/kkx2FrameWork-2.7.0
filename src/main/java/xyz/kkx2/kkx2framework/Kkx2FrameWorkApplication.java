94b5dcfb66925babd6c5a37a85d82b967dde0a11
kx4 <snjune6@naver.com>
2022-06-04 22:29:16 +0900

    프레임워크 최초 생성

---
+++ src/main/java/xyz/kkx2/kkx2framework/Kkx2FrameWorkApplication.java
@@ -0,0 +1,13 @@
+package xyz.kkx2.kkx2framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kkx2FrameWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kkx2FrameWorkApplication.class, args);
+    }
+
+}
